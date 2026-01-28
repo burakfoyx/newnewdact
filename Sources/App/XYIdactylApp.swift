d6ad84d9b7e48f0830e2b2683b2de27f853603f6
XYIdactyl Developer <dev@xyidactyl.app>
2026-01-28 03:18:02 +0300

    Trigger Local Network permission on launch and update Info.plist

--- Sources/App/XYIdactylApp.swift
+++ Sources/App/XYIdactylApp.swift
@@ -2,6 +2,14 @@ import SwiftUI
 
 @main
 struct XYIdactylApp: App {
+    init() {
+        // Attempt to trigger Local Network permission early if the user is going to use a local IP
+        // by making a dummy outgoing request to a private IP range.
+        let url = URL(string: "http://192.168.0.1")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 1)
+        URLSession.shared.dataTask(with: request).resume()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
